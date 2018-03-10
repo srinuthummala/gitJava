2afd9a68bf6d891d600603f927b4cbb0496ab19b
srinuthummala <srieceds@gmail.com>
2018-03-10 16:58:42 +0530

    first commit

---
+++ src/git/GitJava.java
@@ -0,0 +1,12 @@
+package git;
+
+public class GitJava {
+
+	public void method1() {
+		System.out.println("HI I AM IN METHOD1");
+	}
+	
+	public void method2() {
+		System.out.println("HI I AM IN METHOD2");
+	}
+}
---
+++ src/git/GitMain.java
@@ -0,0 +1,11 @@
+package git;
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		GitJava gj = new GitJava();
+		gj.method1();
+		gj.method2();
+	}
+
+}
